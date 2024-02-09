c87a2f550d2040401da58c957b6ded72112b05cc
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-02-09 14:35:19 +0000

    feat: Saving a User in the DB (#4)

--- StudentPortal/src/main/java/com/sesc/studentportal/controller/UserController.java
+++ StudentPortal/src/main/java/com/sesc/studentportal/controller/UserController.java
@@ -1,8 +1,24 @@
 package com.sesc.studentportal.controller;
 
+import com.sesc.studentportal.model.User;
+import com.sesc.studentportal.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    //TODO: Implement Controller
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@Valid @RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
--- StudentPortal/src/main/java/com/sesc/studentportal/services/UserService.java
+++ StudentPortal/src/main/java/com/sesc/studentportal/services/UserService.java
@@ -1,5 +1,45 @@
 package com.sesc.studentportal.services;
 
+import com.sesc.studentportal.model.Student;
+import com.sesc.studentportal.model.User;
+import com.sesc.studentportal.repository.StudentRepository;
+import com.sesc.studentportal.repository.UserRepository;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-    // TODO: Implement the UserService
+
+    private final UserRepository userRepository;
+    private final StudentRepository studentRepository;
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findUserByUserName(username);
+    }
+
+    public Student findStudentFromUser(@NotNull User user) {
+        return studentRepository.findStudentByStudentId(user.getUserId());
+    }
+
+    public User createUser(@NotNull User user) {
+        userRepository.save(user);
+        return user;
+    }
+
+
+    // CONSTRUCTOR //
+
+    /***
+     * Constructor for the UserService.
+     * Using AutoWired annotation to inject the UserRepository into the UserService.
+     * @param userRepository the Repository for the User
+     */
+    public UserService(UserRepository userRepository, StudentRepository studentRepository) {
+        this.userRepository = userRepository;
+        this.studentRepository = studentRepository;
+    }
 }
