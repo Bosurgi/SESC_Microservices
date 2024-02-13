8e63493495b91eda001de714e369830c46564aed
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-02-13 12:21:26 +0000

    test: frontend default added (#5)

---
+++ StudentPortal/frontend/routes.tsx
@@ -0,0 +1,19 @@
+import MainLayout from 'Frontend/views/MainLayout.js';
+import {lazy} from 'react';
+import {createBrowserRouter, RouteObject} from 'react-router-dom';
+
+const AboutView = lazy(async () => import('Frontend/views/about/AboutView.js'));
+const HomeView = lazy(async () => import('Frontend/views/helloworld/HelloWorldView.js'));
+
+export const routes = [
+    {
+        element: <MainLayout/>,
+        handle: {title: 'Main'},
+        children: [
+            {path: '/', element: <HomeView/>, handle: {title: 'Hello World'}},
+            {path: '/about', element: <AboutView/>, handle: {title: 'About'}},
+        ],
+    },
+] as RouteObject[];
+
+export default createBrowserRouter(routes);
