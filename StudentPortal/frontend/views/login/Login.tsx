137ead777fd9601b29e08481b1dfaae0e63031c7
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-02-17 13:36:08 +0000

    chore: Removed forgot password (#5)

--- StudentPortal/frontend/views/login/Login.tsx
+++ StudentPortal/frontend/views/login/Login.tsx
@@ -4,7 +4,11 @@ import {NavLink} from "react-router-dom";
 export default function Login() {
     return (
         <div className="flex flex-col items-center justify-center p-5">
-            <LoginForm no-autofocus/>
+            <LoginForm
+                no-autofocus
+                noForgotPassword={true}
+            />
+
             <NavLink to="/register" className="text-primary-500 underline underline-offset-[8px]">Don't have an account?
                 Register
                 here</NavLink>
