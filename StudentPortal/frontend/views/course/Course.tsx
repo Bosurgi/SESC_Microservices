9e6b36bedefdc1c2787563f836e2c8a90031efda
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-02-18 14:51:01 +0000

    feat: Module search implemented #5

--- StudentPortal/frontend/views/course/Course.tsx
+++ StudentPortal/frontend/views/course/Course.tsx
@@ -12,12 +12,21 @@ export default function Course() {
 
     // The module List to be displayed
     const [moduleList, setModuleList] = useState<Module[]>([])
-    // The search term to filter the module list
-    const [searchTerm, setSearchTerm] = useState<string>('');
+    // The new list of modules after filtering
+    const [filteredModules, setFilteredModules] = useState<Module[]>([])
 
-    // Calling the function to populate the module list from the Module Endpoint
+    // Fetch the modules from the backend and filter them
     useEffect(() => {
-        getModules().then((modules) => setModuleList(modules));
+        getModules().then((modules) => {
+            const newModules = modules.map((module) => ({
+                ...module,
+                title: module.title,
+                description: module.description,
+                fee: module.fee,
+            }));
+            setModuleList(newModules);
+            setFilteredModules(newModules);
+        })
     }, []);
 
     return (
@@ -25,15 +34,33 @@ export default function Course() {
             <VerticalLayout className="p-8">
                 <TextField
                     placeholder="Search"
-                    style={{width: '50%'}}
+                    style={{width: '30%'}}
+                    // Filtering the modules based on the search term
+                    onValueChanged={(e) => {
+                        const searchTerm = (e.detail.value.trim() || '').trim().toLowerCase();
+                        setFilteredModules(
+                            // Using only the title to filter the modules
+                            moduleList.filter(
+                                ({title}) =>
+                                    !searchTerm ||
+                                    title?.toLowerCase().includes(searchTerm)
+                            ));
+                    }}
                 >
                     <Icon slot="prefix" icon="vaadin:search"/>
                 </TextField>
 
-                <Grid items={moduleList}>
-                    <GridSortColumn path="title" header="Title"/>
-                    <GridSortColumn path="description" header="Description"/>
-                    <GridSortColumn path="fee" header="Fee"/>
+                <Grid items={filteredModules}>
+                    <GridSortColumn path="title" header="Title" autoWidth resizable/>
+
+                    {/* TODO: On click display the module details */}
+
+                    <GridSortColumn path="description" header="Description" autoWidth resizable/>
+
+                    <GridSortColumn path="fee" header="Fee" autoWidth>
+                        {/* If the fee is not set, display "Free" else add £ symbol*/}
+                        {({item}) => (item.fee ? `£${item.fee}` : 'Free')}
+                    </GridSortColumn>
                 </Grid>
             </VerticalLayout>
         </>
