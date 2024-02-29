e724ac9788e9b83a6baca87b5d60628d94558eef
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-02-29 17:02:27 +0000

    feat: Mapped authorities for rolesAllowed in routes (#15)

--- StudentPortal/frontend/auth.ts
+++ StudentPortal/frontend/auth.ts
@@ -1,7 +1,8 @@
 import {configureAuth} from "@hilla/react-auth";
 import {JpaUserDetailService} from "Frontend/generated/endpoints";
 
-const auth = configureAuth(JpaUserDetailService.getUserInfo);
+const auth = configureAuth(JpaUserDetailService.getUserInfo,
+    {getRoles: (user) => user.authorities.map(a => a.toString())});
 
 export const useAuth = auth.useAuth;
 export const AuthProvider = auth.AuthProvider;
