7b68756e054d330df248321687f8a00f9b1d1123
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-20 16:03:19 +0000

    feat: Book Service methods implemented

--- LibraryService/src/main/java/com/sesc/libraryservice/service/BookService.java
+++ LibraryService/src/main/java/com/sesc/libraryservice/service/BookService.java
@@ -1,7 +1,10 @@
 package com.sesc.libraryservice.service;
 
+import com.sesc.libraryservice.model.Book;
 import com.sesc.libraryservice.repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -11,6 +14,24 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
 
+    public Book findBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book findBookByIsbn(String isbn) {
+        return bookRepository.findBookByIsbn(isbn);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
 
 }
