9c778bac7e8c87b72b35ce2792de3745cebb95a6
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-20 16:04:38 +0000

    feat: Transaction methods to create transaction, update etc. implemented

--- LibraryService/src/main/java/com/sesc/libraryservice/repository/TransactionRepository.java
+++ LibraryService/src/main/java/com/sesc/libraryservice/repository/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.sesc.libraryservice.repository;
 
+import com.sesc.libraryservice.model.Student;
 import com.sesc.libraryservice.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
 
+    Transaction findTransactionByStudent(Student student);
 }
--- LibraryService/src/main/java/com/sesc/libraryservice/service/TransactionService.java
+++ LibraryService/src/main/java/com/sesc/libraryservice/service/TransactionService.java
@@ -1,7 +1,13 @@
 package com.sesc.libraryservice.service;
 
+import com.sesc.libraryservice.model.Book;
+import com.sesc.libraryservice.model.Student;
+import com.sesc.libraryservice.model.Transaction;
 import com.sesc.libraryservice.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Service
 public class TransactionService {
@@ -9,5 +15,37 @@ public class TransactionService {
 
     public TransactionService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
+    }
+
+    /**
+     * It creates a transaction when a student borrows a book.
+     * @param student the student borrowing the book
+     * @param book the book borrowed by the student
+     * @return the transaction created
+     */
+    public Transaction createTransaction(Student student, Book book) {
+        Transaction transaction = new Transaction();
+        LocalDate date = LocalDate.now();
+        transaction.setStudent(student);
+        transaction.setBook(book);
+        transaction.setDateBorrowed(date);
+        return transactionRepository.save(transaction);
+    }
+
+    /**
+     * It updates the transaction when a student returns a book.
+     * @param transaction the transaction to be updated
+     * @return the updated transaction
+     */
+    public Transaction updateTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public Transaction findTransactionByStudent(Student student) {
+        return transactionRepository.findTransactionByStudent(student);
+    }
+
+    public Transaction findTransactionById(Long id) {
+        return transactionRepository.findById(id).orElse(null);
     }
 }
