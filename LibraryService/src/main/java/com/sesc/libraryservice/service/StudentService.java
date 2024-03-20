296121f259d516985e857c1201d9fa1c733e4686
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-20 16:55:11 +0000

    feat: Students can be created if they don't already exist.

--- LibraryService/src/main/java/com/sesc/libraryservice/service/StudentService.java
+++ LibraryService/src/main/java/com/sesc/libraryservice/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.sesc.libraryservice.service;
 
+import com.sesc.libraryservice.constants.LibraryConstants;
+import com.sesc.libraryservice.model.Student;
 import com.sesc.libraryservice.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +13,28 @@ public class StudentService {
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
+
+    /**
+     * Creates a new student with the given studentId.
+     *
+     * @param studentId the ID of the student
+     * @return the created student entity
+     */
+    public Student createStudent(String studentId) throws RuntimeException {
+        // Check if the student already exists
+        if (studentRepository.findStudentByStudentId(studentId) != null) {
+            throw new RuntimeException("Student with ID " + studentId + " already exists");
+        }
+
+        // Create a new student entity
+        Student student = new Student();
+        student.setStudentId(studentId);
+        student.setPassword(LibraryConstants.DEFAULT_PIN.getStringValue());
+        student.setRole(LibraryConstants.STUDENT_ROLE.getStringValue());
+
+        // Save the student entity in the database
+        return studentRepository.save(student);
+    }
+
+
 }
