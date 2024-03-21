b5868010a28ab827f9c4d895742202a27d2db723
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-21 10:47:46 +0000

    feat: Book Controller implemented with CRUD endpoints.

---
+++ LibraryService/src/main/java/com/sesc/libraryservice/controller/BookController.java
@@ -0,0 +1,63 @@
+package com.sesc.libraryservice.controller;
+
+import com.sesc.libraryservice.model.Book;
+import com.sesc.libraryservice.service.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    /**
+     * Get all books from the Database
+     * @return List of Books and HTTP Status code
+     */
+    @GetMapping()
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.findAllBooks();
+        return ResponseEntity.ok(books);
+    }
+
+    /**
+     * Get a book by the ISBN
+     * @param isbn the ISBN to search for
+     * @return the book entity along with HTTP Status code.
+     */
+    @GetMapping("/{isbn}")
+    public ResponseEntity<Book> getBookByIsbn(@PathVariable String isbn) {
+        Book book = bookService.findBookByIsbn(isbn);
+        return ResponseEntity.ok(book);
+    }
+
+    /**
+     * Post request endpoint to add a book to the Database
+     * @param book the book to add
+     * @return the created book entity and HTTP status code.
+     */
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        Book newBook = bookService.saveBook(book);
+        return ResponseEntity.ok(newBook);
+    }
+
+    /**
+     * Delete a book by the ISBN
+     * @param isbn the ISBN to search for
+     * @return the book entity along with HTTP Status code.
+     */
+    @DeleteMapping("/{isbn}")
+    public ResponseEntity<Book> deleteBookByIsbn(@PathVariable String isbn) {
+        Book book = bookService.findBookByIsbn(isbn);
+        bookService.deleteBook(book.getId());
+        return ResponseEntity.ok(book);
+    }
+}
