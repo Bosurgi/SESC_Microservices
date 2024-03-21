99b12538d2799175485da12d44f594df62f3a635
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-21 11:01:22 +0000

    feat: Transaction Controller implemented see comments. comment: Need to modify the transaction business logic to borrow and return books as at the moment they use the same endpoint which is not ideal.

---
+++ LibraryService/src/main/java/com/sesc/libraryservice/controller/TransactionController.java
@@ -0,0 +1,54 @@
+package com.sesc.libraryservice.controller;
+
+import com.sesc.libraryservice.model.Book;
+import com.sesc.libraryservice.model.Student;
+import com.sesc.libraryservice.model.Transaction;
+import com.sesc.libraryservice.service.BookService;
+import com.sesc.libraryservice.service.StudentService;
+import com.sesc.libraryservice.service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+    private final BookService bookService;
+
+    private final StudentService studentService;
+
+    public TransactionController(
+            TransactionService transactionService,
+            BookService bookService,
+            StudentService studentService
+    ) {
+
+        this.transactionService = transactionService;
+        this.bookService = bookService;
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/return")
+    public ResponseEntity<Transaction> returnBook(@RequestParam String studentId, @RequestParam String bookIsbn) {
+        // Fetching Book and Student to create the Transaction
+        Book book = bookService.findBookByIsbn(bookIsbn);
+        Student student = studentService.getStudentById(studentId);
+
+        Transaction transaction = transactionService.createTransaction(student, book);
+        return ResponseEntity.ok(transaction);
+    }
+
+    @PostMapping("/borrow")
+    public ResponseEntity<Transaction> borrowBook(@RequestParam String studentId, @RequestParam String bookIsbn) {
+        // Fetching Book and Student to create the Transaction
+        Book book = bookService.findBookByIsbn(bookIsbn);
+        Student student = studentService.getStudentById(studentId);
+        // TODO: Implement different service Logic for borrowing
+        Transaction transaction = transactionService.createTransaction(student, book);
+        return ResponseEntity.ok(transaction);
+    }
+}
