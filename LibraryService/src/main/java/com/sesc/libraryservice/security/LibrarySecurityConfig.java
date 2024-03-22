1fb4d3e49bb2509d20a307c6a394177968673132
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-22 12:23:00 +0000

    feat: SecurityConfig set up to accept requests (#26)

---
+++ LibraryService/src/main/java/com/sesc/libraryservice/security/LibrarySecurityConfig.java
@@ -0,0 +1,61 @@
+package com.sesc.libraryservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class LibrarySecurityConfig {
+
+
+    private LibraryUserDetailsService userDetailsService;
+
+    @Autowired
+    public LibrarySecurityConfig(LibraryUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/", "/home").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+
+
+//                .formLogin((form) -> form
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
