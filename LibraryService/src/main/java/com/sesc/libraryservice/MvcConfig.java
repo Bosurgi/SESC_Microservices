4a6f673898c8b2fbc4c6473b5a84165a984d721c
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-03-28 14:49:44 +0000

    feat: Admin can see All Students,bookings and overdue books (#27).

--- LibraryService/src/main/java/com/sesc/libraryservice/MvcConfig.java
+++ LibraryService/src/main/java/com/sesc/libraryservice/MvcConfig.java
@@ -16,6 +16,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/api/v1/transactions").setViewName("borrow");
         registry.addViewController("/api/v1/transactions").setViewName("account");
         registry.addViewController("/api/v1/transactions/return").setViewName("return");
+        registry.addViewController("/api/v1/students").setViewName("admin/students");
 
     }
 }
