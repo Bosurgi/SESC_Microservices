029647ec3ddb28619065e327df4c8a6e68c1a1e3
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-04-06 18:15:00 +0100

    feat: Implemented Eureka Client

--- finance/src/main/java/uk/ac/leedsbeckett/finance/FinanceApplication.java
+++ finance/src/main/java/uk/ac/leedsbeckett/finance/FinanceApplication.java
@@ -2,8 +2,10 @@ package uk.ac.leedsbeckett.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication //provides component scanning, autoconfiguration and property support
+@EnableDiscoveryClient //enables service registration and discovery
 public class FinanceApplication {
 
     public static void main(String[] args) {
