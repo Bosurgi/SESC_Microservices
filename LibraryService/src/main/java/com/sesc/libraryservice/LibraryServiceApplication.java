b896b3bdbf2c5a729b0fd26a42ac1946dfebca46
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-04-06 18:19:48 +0100

    feat: Discovery Client enabled in Library Microservice.

--- LibraryService/src/main/java/com/sesc/libraryservice/LibraryServiceApplication.java
+++ LibraryService/src/main/java/com/sesc/libraryservice/LibraryServiceApplication.java
@@ -9,12 +9,14 @@ import com.sesc.libraryservice.repository.TransactionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LibraryServiceApplication {
 
     public static void main(String[] args) {
