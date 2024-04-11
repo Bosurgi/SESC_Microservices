18b67bb896ee9c502d67110da0cf6945129dc3c8
A. La Fauci <andrea.lafauci92@gmail.com>
2024-04-11 17:24:07 +0200

    feat: ContextLoad Test working and configuration added.

--- StudentPortal/src/test/java/com/sesc/studentportal/StudentPortalApplicationTests.java
+++ StudentPortal/src/test/java/com/sesc/studentportal/StudentPortalApplicationTests.java
@@ -1,9 +1,14 @@
 package com.sesc.studentportal;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@ContextConfiguration(classes = TestConfig.class)
+@TestPropertySource(locations = "classpath:application.properties")
+@DataJpaTest
 class StudentPortalApplicationTests {
 
 	@Test
